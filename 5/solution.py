04a93743874ade4d044d8a0e8f7640c9907c0e2e
Laura <lauracorssac@gmail.com>
2021-12-19 21:06:37 -0300

    problem 2 and 1

--- 5/solution.py
+++ 5/solution.py
@@ -1,28 +1,40 @@
-
-
 def split_coordinate(coordinate):
     components = coordinate.split(',')
     return (int(components[0]), int(components[1]))
 
-def update_dic(dic, n_intersections, common_axis, common_axis_value, diff_axis_0, diff_axis_1):
-    increment = -1 if diff_axis_0 > diff_axis_1 else 1
+def get_increment(axis0, axis1):
+    if axis0 == axis1:
+        return 0
+    if axis0 > axis1:
+        return -1
+    return 1
+
+def update_dic(dic, n_intersections, x0, x1, y0, y1):
+    increment_x = get_increment(x0, x1)
+    increment_y = get_increment(y0, y1)
+
+    inc_x = increment_x if increment_x != 0 else 1
+    inc_y = increment_y if increment_y != 0 else 1
     
-    for i in range(diff_axis_0, diff_axis_1 + increment, increment):
-        
-        x = common_axis_value if common_axis == 'x' else i
-        y = common_axis_value if common_axis == 'y' else i
+    x = x0
+    y = y0
+
+    while x in range(x0, x1 + increment_x, inc_x) or y in range(y0, y1 + increment_y, inc_y):
+
         covered_coordinate = "{x},{y}".format(x= x, y= y)
-        
+    
         if covered_coordinate in dic:
             if dic[covered_coordinate] == 1:
                 n_intersections += 1
             dic[covered_coordinate] += 1
         else:
             dic[covered_coordinate] = 1
+
+        x += increment_x
+        y += increment_y
     return n_intersections
 
-
-def get_number_intersections(lines):
+def get_number_intersections(lines, problem_code):
     dic = {}
     n_intersections = 0
     for line in lines:
@@ -30,27 +42,14 @@ def get_number_intersections(lines):
         x0, y0 = split_coordinate(components[0])
         x1, y1 = split_coordinate(components[1])
 
-        if x0 == x1:
-            n_intersections = update_dic(dic, n_intersections, 'x', x0, y0, y1)
-        elif y0 == y1:
-            n_intersections = update_dic(dic, n_intersections, 'y', y0, x0, x1)
-    
+        if x0 != x1 and y0 != y1 and not (problem_code == 'p2' and (abs(x0 - x1) == abs(y0 - y1))):
+            continue
+
+        n_intersections = update_dic(dic, n_intersections, x0, x1, y0, y1)
+       
     return n_intersections
-
-
-
 
 file = open('input.txt', 'r')
 lines = file.readlines()
-print("Problem 1 = ", get_number_intersections(lines))
-
-
-
-
-
-    
-
-
-
-
-
+print("Problem 1 = ", get_number_intersections(lines , 'p1'))
+print("Problem 2 = ", get_number_intersections(lines , 'p2'))
