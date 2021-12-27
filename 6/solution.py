1953e369c955447f64905abb9b31a84e5223808c
Laura <lauracorssac@gmail.com>
2021-12-27 20:18:50 -0300

    solution day 6

--- 6/solution.py
+++ 6/solution.py
@@ -1,39 +1,46 @@
-
-def find_total(fishes, final_day):
-
-    if not fishes: 
-        return 0
-
-    fish_age = fishes[0][0]
-    fish_birthday = fishes[0][1]
-    quantity = fishes[0][2]
-    day_next_birth = fish_birthday + fish_age + 1
-
-    children_counter = 0
-    while day_next_birth <= final_day:
-        children_counter += quantity
-        fishes.append((8,day_next_birth, quantity))
-        day_next_birth += 7
-    
-    return children_counter + find_total(fishes[1:], final_day)
-    
-
-
-def pre_process_input(initial_ages):
+def build_dic(initial_ages):
     dic = {}
     for age in initial_ages:
         if age not in dic:
             dic[age] = 1
         else:
             dic[age] += 1
-    return [(x, 0, dic[x]) for x in dic]
+    return dic
+
+def pre_process_input(initial_ages, final_day, fish_counter_list):
+    dic = build_dic(initial_ages)
+    print(dic)
+    for fish_age in dic:
+        quantity = dic[fish_age]
+        day_next_birth = fish_age + 1
+        while day_next_birth <= final_day:
+            fish_counter_list[day_next_birth] += quantity
+            day_next_birth += 7
+    
+    return fish_counter_list
+
+def find_total(final_day, fish_counter_list):
+
+    for day in range(1, final_day + 1):
+        quantity = fish_counter_list[day]
+        if quantity == 0: 
+            continue
+        day_next_birth = day + 8 + 1
+        while day_next_birth <= final_day:
+            fish_counter_list[day_next_birth] += quantity
+            day_next_birth += 7
+    return fish_counter_list
         
 file = open('input.txt', 'r')
 line = file.readline()
 initial_ages = list(map(int, line.split(',')))
-final_day = 30
+final_day = 256
 initial_ages_len = len(initial_ages)
-initial_ages = pre_process_input(initial_ages)
-print(initial_ages)
-print("Problem 1 = ",  initial_ages_len + find_total(initial_ages, final_day))
+fishes = [0] * (final_day + 1)
+fishes[0] = initial_ages_len
+fishes = pre_process_input(initial_ages, final_day, fishes)
+print(fishes)
+fishes = find_total(final_day, fishes)
+print(fishes)
+print("Problem 1 = ", sum(fishes))
 
