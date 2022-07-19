303e17d5debf5b7419b6ae24d673749509f24233
Laura <lauracorssac@gmail.com>
2022-07-19 14:41:57 -0300

    day 14 improvements

--- 14/solution.py
+++ 14/solution.py
@@ -1,5 +1,6 @@
 import queue
 import sys
+import time
 
 MIN_TEMPLATE_LENGTH = 2
 
@@ -38,13 +39,69 @@ def get_difference_max_min(char_dic):
             max_occurence = occurence
     
     return max_occurence - min_occurence
+    
+# an effitient alternative for solution 1
+# space = pairs_dic + step_pairs + char_counter_dic
+# O(26*26) + O(26*26) + O(1)
+# the maximum number of pairs possible is 26*26 since there is only 26 letters in the alphabet
 
+# if we couldnt assume that only capital letters were used...
+# space = pairs_dic + step_pairs + char_counter_dic
+# O(x) + O(x) + O(n)  where x is the number of rules and n the number of letters
+# O(n^2) + O(n^2) + O(n) = O(n^2)
+
+# time = O(n) + O(m) where n is the initial word size and m is the number of steps
+
+def solution_2(file, steps):
+    initial_chars = file.readline().strip()
+    file.readline()
+    pairs_dic = make_pairs_dic(file)
+    
+    if len(initial_chars) < MIN_TEMPLATE_LENGTH:
+        raise
+        
+    char_counter = {}
+    step_pairs = {}
+    
+    for ind, char in enumerate(initial_chars[:-1]):
+        insert_char_on_dic(char_counter, char)
+        insert_char_on_dic(step_pairs, char + initial_chars[ind + 1])
+    insert_char_on_dic(char_counter, initial_chars[-1])
+    
+    
+    for _ in range(steps):
+        
+        next_step_pairs = {}
+        
+        for pair, occurences in step_pairs.items():
+            new_letter = pairs_dic[pair]
+            new_pair_0 = pair[0] + new_letter
+            new_pair_1 = new_letter + pair[1]
+            
+            char_counter[new_letter] = char_counter.get(new_letter, 0) + occurences
+            next_step_pairs[new_pair_0] = next_step_pairs.get(new_pair_0, 0) + occurences
+            next_step_pairs[new_pair_1] = next_step_pairs.get(new_pair_1, 0) + occurences
+            
+        step_pairs = next_step_pairs
+        
+    return get_difference_max_min(char_counter)
+            
+        
+# inefficient solution. Only works for small number of steps
+# space complex = char_counter + pairs_queue
+# O(n) + O(final_number_gets)
+# O(n) + O( (n-1) * 2^x - (n-1) )
+# O(n) + O( n * 2^x ) =  O( n * 2^x )
+
+# time = O( n * 2^x )
 
 def solution_1(file, steps):
     initial_chars = file.readline().strip()
     file.readline()
     pairs_dic = make_pairs_dic(file)
     final_number_gets = get_number_gets(initial_chars, steps)
+    print("final number gets = ", final_number_gets)
+    time.sleep(5)
     
     if len(initial_chars) < MIN_TEMPLATE_LENGTH:
         raise
@@ -56,17 +113,24 @@ def solution_1(file, steps):
     for char in initial_chars:
         insert_char_on_dic(char_counter, char)
 
+    pairs_put = 0
     pairs_gotten = 0
     for ind, char in enumerate(initial_chars[:-1]):
         pairs_queue.put(char + initial_chars[ind + 1])
+        pairs_put += 1
             
     while pairs_gotten < final_number_gets:
         pair = pairs_queue.get()
-        new_letter = pairs_dic[pair]
-        pairs_queue.put(pair[0]+new_letter)
-        pairs_queue.put(new_letter+pair[1])
-        insert_char_on_dic(char_counter, new_letter)
         pairs_gotten += 1
+        print("getting", pairs_gotten)
+        new_letter = pairs_dic[pair]
+        insert_char_on_dic(char_counter, new_letter)
+        
+        if pairs_put < final_number_gets:
+            pairs_queue.put(pair[0]+new_letter)
+            pairs_queue.put(new_letter+pair[1])
+            pairs_put += 2
+            
     
     return get_difference_max_min(char_counter)
     
